2393d86f5e43a0b4bdefd0c17858aa01e6c86d0b
Hamit Mizrak <hamitmizrak@gmail.com>
2024-05-07 13:07:34 +0300

    RegisterDto

---
+++ src/main/java/com/hamitmizrak/annotation/UniqueEmailAddressValidation.java
@@ -0,0 +1,32 @@
+package com.hamitmizrak.annotation;
+
+import com.hamitmizrak.fullstackdeveloper12.data.repository.IRegisterRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+
+// LOMBOK
+@RequiredArgsConstructor
+
+// ConstraintValidator
+public class UniqueEmailAddressValidation implements ConstraintValidator<AnnotationUniqueEmailAddress,String> {
+
+    // INJECTION
+     private final IRegisterRepository iRegisterRepository;
+
+    @Override
+    public void initialize(AnnotationUniqueEmailAddress constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    // DATABASE SORGUSU
+    @Override
+    public boolean isValid(String emailAddress, ConstraintValidatorContext constraintValidatorContext) {
+        Boolean isEmailAddress=iRegisterRepository.findByRegisterEmail(emailAddress).isPresent();
+        //Eğer email address sistemde varsa
+        if(isEmailAddress){
+            return false;
+        }
+        return true; //email sistemde yoksa
+    } //end isValid
+} //end class
---
+++ src/main/java/com/hamitmizrak/business/dto/RegisterDto.java
@@ -0,0 +1,84 @@
+package com.hamitmizrak.business.dto;
+
+import com.hamitmizrak.annotation.AnnotationUniqueEmailAddress;
+import com.hamitmizrak.audit.AuditingAwareBaseDto;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+
+// LOMBOK
+@Data // @Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+@Builder
+// @SneakyThrows
+// RoleDto(M) RegisterDto(N)
+
+public class RegisterDto extends AuditingAwareBaseDto implements Serializable {
+
+    // Serilestirme
+    public static final Long serialVersionUID=1L;
+
+    // Register ID
+    private Long registerId;
+
+    // System Created Date
+    private Date systemCreatedDate;
+
+    // Nickname
+    @NotEmpty(message = "{register.nickname.validation.constraints.NotNull.message}")
+    private String nickName;
+
+    // Name
+    @NotEmpty(message = "{register.name.validation.constraints.NotNull.message}")
+    private String name;
+
+    // Surname
+    @NotEmpty(message = "{register.surname.validation.constraints.NotNull.message}")
+    private String surname;
+
+    // Email
+    @NotEmpty(message = "{register.email.validation.constraints.NotNull.message}")
+    @Email(message = "{register.email.validation.constraints.regex.message}")
+    @AnnotationUniqueEmailAddress
+    private String email;
+
+    // Password
+    @NotEmpty(message = "{register.password.validation.constraints.NotNull.message}")
+    @Size(min = 7,max=15,  message = "{register.password.validation.constraints.MinMax.NotNull.message }")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).*$", message = "{register.password.pattern.validation.constraints.NotNull.message}")
+    private String password;
+
+    // Page Authorization (O kişi o sayfaya yetkisi var mı
+    @Builder.Default
+    private Boolean pageAuthorization=false;
+
+    // Rolles (Enum)
+    private Collection<RoleDto> roles;
+
+    /////////////////////////////////////////////
+    // USER DETAILS
+    // Kullanıcı üye olurken kilitli olsun ancak mail onayı ile aktifleştirilsin
+    public Boolean isAccountNonLocked;
+
+    // Kullanıcı hesabını pasit duruma getirme
+    public Boolean isAccountNonExpired;
+
+    // Kullanıcı Hesap Bilgi Süresi (Authorization)
+    public Boolean isCredentialsNonExpired;
+
+    // Kullanıcı Sistemde mi ?
+    public Boolean isEnabled;
+
+} //end Register
